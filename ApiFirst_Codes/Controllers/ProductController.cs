ef70a8bb973eabdc6da943fe51aa14ff4f9eee5b
HamidzadeTural913 <tu76fe6rc@code.edu.az>
2022-06-22 05:28:52 +0400

    APIFirst

---
+++ ApiFirst_Codes/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using ApiFirst_Codes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiFirst_Codes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private List<Product> _products = new List<Product>()
+        {
+            new Product
+            {
+                Id=1,
+                Name="Cup",
+                Price = 1.5m
+            },
+
+            new Product
+            {
+                Id=2,
+                Name="Monitor",
+                Price = 400.55m
+            },
+
+            new Product
+            {
+                Id=3,
+                Name="TV",
+                Price = 500.99m
+            },
+
+            new Product
+            {
+                Id=4,
+                Name="Carpet",
+                Price = 234.5m
+            }
+        };
+        [HttpGet("get/{id?}")]
+       
+        public IActionResult Get(int id)
+        {
+            Product product = _products.FirstOrDefault(p=>p.Id==id);
+            if( product==null) return NotFound();
+            return Ok(product);
+        }
+        [HttpGet("all")]
+
+        public IActionResult GetAll()
+        {
+            return Ok(_products);
+        }
+ 
+    }
+}
---
+++ ApiFirst_Codes/DAL/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiFirst_Codes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFirst_Codes.DAL
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }    
+    }
+}
